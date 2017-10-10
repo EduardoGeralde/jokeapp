7217c988e21912eb188483258548a7a80482d423
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-09 21:30:55 -0300

    chucknorris dependency added

---
+++ src/main/java/com/eduardoportfolio/joke/controllers/JokeController.java
@@ -0,0 +1,30 @@
+package com.eduardoportfolio.joke.controller;
+
+import com.eduardoportfolio.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Eduardo on 09/10/17.
+ */
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
+
---
+++ src/main/java/com/eduardoportfolio/joke/services/JokeService.java
@@ -0,0 +1,8 @@
+package com.eduardoportfolio.joke.services;
+
+/**
+ * Created by Eduardo on 09/10/17.
+ */
+public interface JokeService {
+    String getJoke();
+}
